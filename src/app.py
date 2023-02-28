e2a893e46b71518a78d6a54768ebdb148b06b354
William Chávez Díaz <chavezdiaz4@gmail.com>
2023-02-28 00:08:53 -0500

    API Python

---
+++ src/app.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/flaskmysql'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(70), unique=True)
+    description = db.Column(db.String(100))
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+
+# Esta funcion lee todas nuestras clases y a partir de ello crea nuestras tablas
+db.create_all()
+
+
+class TaskSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description')
+
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    title = request.json['title']
+    description = request.json['description']
+
+    new_task = Task(title, description)
+    db.session.add(new_task)
+    db.session.commit()
+
+    return task_schema.jsonify(new_task)
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    all_tasks = Task.query.all()
+    result = tasks_schema.dump(all_tasks)
+    return jsonify(result)
+
+
+@app.route('/tasks/<id>', methods=['GET'])
+def get_task(id):
+    task = Task.query.get(id)
+    return task_schema.jsonify(task)
+
+
+@app.route('/tasks/<id>', methods=['PUT'])
+def update_task(id):
+
+    task = Task.query.get(id)
+
+    title = request.json['title']
+    description = request.json['description']
+
+    task.title = title
+    task.description = description
+
+    db.session.commit()
+    return task_schema.jsonify(task)
+
+
+@app.route('/tasks/<id>', methods=['DELETE'])
+def delete_task(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    
+    return task_schema.jsonify(task)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify('Welcome to my API development with Python, Flask, MySQL & SQLAlchemy - @wcdz')
+
+if __name__ == "__main__":
+    app.run(debug=True)
